09f775dcd5b43813cf368652d001205a1122a83f
leoimpett <leoimpett@googlemail.com>
2019-03-21 10:47:09 +0100

    add first batch of files

---
+++ makePoseJsons.sh
@@ -0,0 +1,13 @@
+
+OPENPOSEDIR='/home/leonardo/Documents/GPUcomputing/openpose'
+
+
+
+WRITEIMDIR='./pose-json/'
+
+IMDIR=$1
+
+rm $WRITEIMDIR/*
+
+$OPENPOSEDIR/build/examples/openpose/openpose.bin -face -hand  -model_folder $OPENPOSEDIR/models --image_dir $IMDIR --write_json $WRITEIMDIR   -render_pose 0 -display 0
+
